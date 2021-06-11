bfbab0c2b574c8edd3191a047f3f163aa69c1b57
Elisa Shapiro <elisashapiro@gmail.com>
2021-06-11 14:13:56 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(name) {
+    return cats.push('Ralph');
+}
+cats;
+function destructivelyPrependCat(name) {
+    return cats.unshift('Bob');
+}
+cats;
+function destructivelyRemoveLastCat(name) {
+    return cats.pop('Garfield');
+}
+cats;
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift('Milo');
+}
+function appendCat(name) {
+    const copyOfCats = [...cats, 'Broom'];
+    return copyOfCats;
+}
+copyOfCats;
+function prependCat(name) {
+    const copyOfCats = ['Arnold', ...cats];
+    return copyOfCats;
+}
+function removeLastCat(name) {
+    return cats.slice(0, cats.length-1);
+}
+cats;
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
+cats;
